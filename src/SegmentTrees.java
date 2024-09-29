320a23a189e165d085f05c60ec0646bd52ab81a9
tuhin <tuhinm2002@gmail.com>
2024-09-29 19:26:43 +0530

    segment trees working

--- src/SegmentTrees.java
+++ src/SegmentTrees.java
@@ -5,12 +5,49 @@ public class SegmentTrees {
         private int endIndex;
         private Node left;
         private Node right;
+        private int data;
 
-        public Node(){
-
+        public Node(int startIndex,int endIndex){
+            this.startIndex = startIndex;
+            this.endIndex = endIndex;
         }
     }
 
     private Node root;
-    
+
+    public Node insert(int[] arr,int start,int end){
+        if(start == end){
+            Node node = new Node(start,end);
+            node.data = arr[start];
+            return node;
+        }
+
+        Node newNode = new Node(start,end);
+        int mid = (start + end) / 2 ;
+
+        newNode.left = insert(arr,start,mid);
+        newNode.right = insert(arr,mid+1,end);
+
+        newNode.data = newNode.left.data + newNode.right.data;
+
+        return newNode;
+    }
+
+    public void display(Node node){
+        if(node == null){
+            System.out.println("Tree ended");
+            return;
+        }
+        System.out.println("Data value : "+node.data);
+        System.out.println("Start index : "+node.startIndex+" End index : "+node.endIndex);
+        display(node.left);
+        display(node.right);
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1,2,4,8,6,3,10};
+        SegmentTrees segmentTrees = new SegmentTrees();
+        Node node = segmentTrees.insert(arr,0,arr.length-1);
+        segmentTrees.display(node);
+    }
 }
